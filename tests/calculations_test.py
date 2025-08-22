bb4ac0c094c4736dfca38c3e28fa8e6330b8d7bf
Warren Wong <warwwc@gmail.com>
2025-08-22 02:13:47 +0000

    fix

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Installed Modules
-# None
+import pytest
 
 # Project Modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
@@ -69,20 +69,16 @@ def test_get_nth_fibonacci_ten():
     # Assert
     assert result == 55
 
-def test_area_of_circle_negative_radius(self):
-   """Test with a negative radius to raise ValueError."""
-   # Arrange
-   radius = -1
 
-   # Act & Assert
-   with self.assertRaises(ValueError):
-      area_of_circle(radius)
+def test_area_of_circle_negative_radius():
+    """Test with a negative radius to raise ValueError."""
+    radius = -1
+    with pytest.raises(ValueError):
+        area_of_circle(radius)
 
-def test_get_nth_fibonacci_negative(self):
-   """Test with a negative number to raise ValueError."""
-   # Arrange
-   n = -1
 
-   # Act & Assert
-   with self.assertRaises(ValueError):
-      get_nth_fibonacci(n)
+def test_get_nth_fibonacci_negative():
+    """Test with a negative number to raise ValueError."""
+    n = -1
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(n)
